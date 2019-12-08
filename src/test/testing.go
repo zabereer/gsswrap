f887816b5da47fb4b82fe6abba72f4a9bda04ac9
Albert Pretorius <pretoalb@gmail.com>
2019-12-08 14:37:13 +0000

    go for go

--- src/test/testing.go
+++ src/test/testing.go
@@ -1,11 +1,55 @@
 package main
 
 // #cgo LDFLAGS: -lgsswrap
+// #include "gsswrap/client.h"
+// #include "gsswrap/server.h"
 import "C"
 import (
-	"fmt"
+	"flag"
+	"log"
+	"net"
 )
 
 func main() {
-	fmt.Println("hello from go")
+	server := flag.Bool("server", false,
+		"set to run as server, false to run as client")
+	addr := flag.String("address", "",
+		"network address (host:port)")
+
+	if *server {
+		runServer(addr)
+	} else {
+		runClient(addr)
+	}
+}
+
+var con net.Conn
+
+func runServer(addr *string) {
+	log.Print("Running as server on ", *addr)
+	listener, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatal("Error trying to listen on ", *addr, " - ", err)
+	}
+
+	for {
+		con, err = listener.Accept()
+		if err != nil {
+			log.Fatal("Failed to accept - ", err)
+		}
+
+		con.Close()
+	}
+}
+
+func runClient(addr *string) {
+	log.Print("Running as client on ", *addr)
+	c, err := net.Dial("tcp", *addr)
+	if err != nil {
+		log.Fatal("Error trying to ", *addr, " - ", err)
+	}
+
+	con = c
+
+	con.Close()
 }
